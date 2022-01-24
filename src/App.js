6d0e3e8e7c464409431f7ccecfcf44a721626dd8
Afiz Savage <savageafiz9@gmail.com>
2022-01-24 19:25:03 +0000

    Add routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
+import { Route, Switch, BrowserRouter as Router } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello world!</h1>
-    </div>
+    <Router>
+      <header>header</header>
+      <Switch>
+        <Route exact path="/">
+          <h1>home</h1>
+        </Route>
+        <Route path="/books">
+          <h1>Books</h1>
+        </Route>
+        <Route path="/categories">
+          <h1>categories</h1>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
