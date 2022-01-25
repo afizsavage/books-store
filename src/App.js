574160920d5505e54c86b4d52e300357f3f3c5af
Afiz Savage <savageafiz9@gmail.com>
2022-01-25 08:08:19 +0000

    Add navigation links

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import { Route, Switch, BrowserRouter as Router } from 'react-router-dom';
 import './App.css';
+import Header from './components/header';
 
 function App() {
   return (
     <Router>
-      <header>header</header>
+      <Header />
       <Switch>
         <Route exact path="/">
           <h1>home</h1>
-        </Route>
-        <Route path="/books">
-          <h1>Books</h1>
         </Route>
         <Route path="/categories">
           <h1>categories</h1>
