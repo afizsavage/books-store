f2e97ef76a2b99c9996f36e00a3b209f3bb99b67
Afiz Savage <savageafiz9@gmail.com>
2022-01-26 16:30:40 +0000

    Update UI to reflect state change

--- src/components/books/booklist.js
+++ src/components/books/booklist.js
@@ -1,37 +1,23 @@
+import { useSelector } from 'react-redux';
+
 import BookCard from './card';
 
-const books = [
-  {
-    id: 0,
-    author: 'Suzanne Collins',
-    category: 'Action',
-    title: 'The Hunger Games',
-  },
-  {
-    id: 1,
-    author: 'Julie Carwood',
-    category: 'Action',
-    title: 'Shadow Dance',
-  },
-  {
-    id: 2,
-    author: 'Jeffery Deaver',
-    category: 'Thriller',
-    title: 'Shallow Graves',
-  },
-];
+const BookList = () => {
+  const books = useSelector((state) => state.booksReducer);
 
-const BookList = () => (
-  <div>
-    <ul>
-      {books.map((book) => (
-        <li key={book.id}>
-          {' '}
-          <BookCard book={book} />
-        </li>
-      ))}
-    </ul>
-  </div>
-);
+  return (
+    <div>
+      <ul>
+        {books.length > 0
+          ? books.map((book) => (
+            <li key={book.id}>
+              <BookCard book={book} />
+            </li>
+          ))
+          : null}
+      </ul>
+    </div>
+  );
+};
 
 export default BookList;
