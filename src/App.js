833dfb5c05ee6505235ce3098d43798f3e9f2a9d
Afiz Savage <savageafiz9@gmail.com>
2022-01-27 12:49:37 +0000

    Fecth data from api when page loades

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
+import { useEffect } from 'react';
 import { Route, Switch, BrowserRouter as Router } from 'react-router-dom';
+import axios from 'axios';
+import { useDispatch } from 'react-redux';
+// import nextId from 'react-id-generator';
+
 import './App.css';
 import Home from './components/books';
 import Header from './components/header';
+import { addBook } from './redux/books/books';
 
-function App() {
+const baseAPI = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/NVqSC6UJSOssSHkbvuAm';
+
+const updateReduxStore = (books, dispatch) => {
+  Object.entries(books).forEach((book) => dispatch(addBook({ ...book[1][0], id: book[0] })));
+};
+
+const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    axios.get(`${baseAPI}/books`).then((books) => updateReduxStore(books.data, dispatch));
+  }, []);
+
   return (
     <Router>
       <Header />
@@ -17,6 +35,6 @@ function App() {
       </Switch>
     </Router>
   );
-}
+};
 
 export default App;
